10ba2985d8bc4a1975a08a25c8e11e8333cdfb76
Ralph.Vitug <rralph@rrc.ca>
2025-10-03 19:53:21 -0500

    added elements and eventListeners

--- script.js
+++ script.js
@@ -35,3 +35,18 @@ const questions = [
     answer: "Walleye"
   }
 ];
+
+// Elements
+
+const questionEl = document.getElementById("question");
+const answersEl = document.getElementById("answers");
+const startBtn = document.getElementById("start-btn");
+const nextBtn = document.getElementById("next-btn");
+const exitBtn = document.getElementById("exit-btn");
+const baitsLeftEl = document.getElementById("baits-left");
+const fishCaughtEl = document.getElementById("fish-caught");
+
+// Event Listeners
+
+startBtn.addEventListener("click", startGame);
+if (exitBtn) exitBtn.addEventListener("click", resetGame);

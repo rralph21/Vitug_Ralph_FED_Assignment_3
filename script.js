c9b84786b9d68a8d85d2fd1269a6e77ff87a7096
Ralph.Vitug <rralph@rrc.ca>
2025-10-03 20:11:54 -0500

    completed .js -- debugging

--- script.js
+++ script.js
@@ -50,3 +50,111 @@ const fishCaughtEl = document.getElementById("fish-caught");
 
 startBtn.addEventListener("click", startGame);
 if (exitBtn) exitBtn.addEventListener("click", resetGame);
+
+// Function - Start
+
+function startGame() {
+  gameActive = true;
+  startBtn.style.display = "none";
+  if (exitBtn) exitBtn.hidden = true;
+
+  currentQuestionIndex = 0;
+  baitsLeft = 5;
+  streak = 0;
+  fishCaught = 0;
+
+  updateStatus();
+  renderQuestion();
+}
+
+// Render Question
+
+function renderQuestion() {
+  // End conditions
+  if (baitsLeft <= 0 || currentQuestionIndex >= questions.length) {
+    return endGame(streak >= 2);
+  }
+
+  // Show current question
+  const q = questions[currentQuestionIndex];
+  questionEl.textContent = q.q;
+
+  // Clear old answers
+  answersEl.innerHTML = "";
+
+  // Build new answer buttons
+  q.options.forEach(option => addAnswerButton(option));
+}
+
+// Answer - Function and conditions
+
+function addAnswerButton(label) {
+  const li = document.createElement("li");
+  const btn = document.createElement("button");
+  btn.textContent = label;
+
+  // When clicked, evaluate the answer
+  btn.addEventListener("click", () => handleAnswer(label));
+
+}
+
+function handleAnswer(selected) {
+  if (!gameActive) return;
+
+  const correct = questions[currentQuestionIndex].answer;
+
+  // Each answer uses up one bait
+  baitsLeft--;
+
+  if (selected === correct) {
+    streak++;
+    fishCaught = streak; // Streak
+    alert("Correct! You caught a fish.");
+
+    // Early win if two correct in a row
+    if (streak >= 2) {
+      return endGame(true);
+    }
+
+  } else {
+    streak = 0;
+    fishCaught = 0;
+    alert("Wrong bait. You missed the fish.");
+  }
+
+  currentQuestionIndex++;
+  updateStatus();
+
+  renderQuestion(); 
+}
+
+// End function
+
+function endGame(win) {
+  gameActive = false;
+  answersEl.innerHTML = "";
+
+  if (exitBtn) exitBtn.hidden = false;
+
+  if (win) {
+    questionEl.textContent = "You caught a rare fish and WIN the game!";
+  } else {
+    questionEl.textContent = "You used all your baits without 2 in a row. Game over!";
+  }
+}
+
+// Reset 
+
+function resetGame() {
+  gameActive = false;
+  startBtn.style.display = "inline-block";
+  if (exitBtn) exitBtn.hidden = true;
+
+  questionEl.textContent = "Press \"Start Game\" to begin fishing!";
+  answersEl.innerHTML = "";
+
+  baitsLeftEl.textContent = "5";
+  fishCaughtEl.textContent = "0";
+}
+
+
